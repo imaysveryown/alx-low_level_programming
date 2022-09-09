a699811b89198158b62fb7b1a2b609eb0626b21f
imaysveryown <asante.arnold@yahoo.com>
2022-09-09 14:32:30 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- prints the function in the main
+ *
+ * Description: using the main funtion, this program prints
+ * alphabets in lowercase
+ * Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (char != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
