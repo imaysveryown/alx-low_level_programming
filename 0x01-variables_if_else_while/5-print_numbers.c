4868f82ab823a9f2f307363f07e7eeaa8d7e6bc6
imaysveryown <asante.arnold@yahoo.com>
2022-09-09 14:59:42 -0700

    print num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- prints the string in the function main
+ *
+ * Description: using the main function print base ten numbers
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
