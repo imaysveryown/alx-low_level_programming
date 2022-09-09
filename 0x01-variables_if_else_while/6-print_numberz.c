cb706c7977500b0cf5ae82515c07bb5f08ef46f8
imaysveryown <asante.arnold@yahoo.com>
2022-09-09 16:04:04 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- prints the function in the main
+ *
+ * Description: using the main function print all base ten numbers
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
