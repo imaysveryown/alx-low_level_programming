e5cf0460c2ff71d82a9d730bfc4b0bda9f18fc8b
imaysveryown <asante.arnold@yahoo.com>
2022-09-13 03:36:05 -0700

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- prints the string in the function main
+ *
+ * Description: the main function prints putchar on the screen
+ * Return: 0
+*/
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+	return (0);
+}
